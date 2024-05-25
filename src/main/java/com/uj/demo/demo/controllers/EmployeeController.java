cf251f4de345bbdfdf0a3811d6b7c80c738872d8
AndriiPokora <andpokora@gmail.com>
2024-05-25 19:52:13 +0200

    removed files

--- src/main/java/com/uj/demo/demo/controllers/EmployeeController.java
+++ src/main/java/com/uj/demo/demo/controllers/EmployeeController.java
@@ -1,6 +1,5 @@
 package com.uj.demo.demo.controllers;
 
-import com.uj.demo.demo.models.Coach;
 import com.uj.demo.demo.models.Employee;
 import com.uj.demo.demo.services.EmployeeService;
 import org.springframework.web.bind.annotation.*;
