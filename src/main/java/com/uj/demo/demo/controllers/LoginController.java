310e148cfca57e95f7b00c62625c2856e91c52b9
AndriiPokora <andpokora@gmail.com>
2024-05-25 21:18:24 +0200

    pages added

--- src/main/java/com/uj/demo/demo/controllers/LoginController.java
+++ src/main/java/com/uj/demo/demo/controllers/LoginController.java
@@ -1,15 +1,22 @@
 package com.uj.demo.demo.controllers;
 
-import com.uj.demo.demo.models.Order;
-import com.uj.demo.demo.repositories.OrderRepository;
+import com.uj.demo.demo.services.LoginService;
+import com.uj.demo.demo.models.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class LoginController {
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
 
     @GetMapping("/login")
-    public String login() {return "login";}
+    public String login() { return "login"; }
 
-
+//    @PostMapping
+//    public User addUser(@RequestBody User user) { return loginService.saveUser(user); }
 }
 
