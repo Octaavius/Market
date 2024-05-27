da569403d6225f4e4951a5c7581a819fb1d36f52
AndreRab <andr.max2003@gmail.com>
2024-05-27 13:50:37 +0200

    Products images added

--- src/main/java/com/uj/demo/demo/controllers/ProductController.java
+++ src/main/java/com/uj/demo/demo/controllers/ProductController.java
@@ -4,6 +4,7 @@ import com.uj.demo.demo.models.Product;
 import com.uj.demo.demo.services.ProductService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,8 +16,17 @@ public class ProductController {
         this.productService = productService;
     }
 
+//    @PostMapping
+//    public Product addProduct(@RequestBody Product product) { return productService.save(product); }
+
     @PostMapping
-    public Product addProduct(@RequestBody Product product) { return productService.save(product); }
+    public List<Product> addProduct(@RequestBody List<Product> products) {
+        List<Product> addedProducts = new ArrayList<>();
+        for(Product product: products){
+            addedProducts.add((productService.save(product)));
+        }
+        return addedProducts;
+    }
 
     @GetMapping("/{name}")
     public List<Product> getProductsByName(@PathVariable String name) {
